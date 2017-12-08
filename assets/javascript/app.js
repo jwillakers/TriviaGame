66d9274b51d52bfec9e192832204c60cc3aa28f4
Jennifer Williams <jwillakers@hotmail.com>
2017-12-07 22:55:05 -0600

    final

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+	// the script is triggered when th button is clicked
+	// start and restart buttons to change screens
+onclick="location.href='game.html'";
+onclick="location.href='index.html'";
+
+});
+// it collects the data clicked from the radio button that user clicks
+
+var correctAnswers = 0;
+var wrongAnswers = 0;
+// first question
+var array1=["#1", "#2", "#3", "#4"];
+
+for(var i = 0; i< array1.length; i ++);
+	if(array1[3].clicked) {
+		console.log(true)
+
+	
+ } else {
+	console.log(false)
+
+var counter = 0
+$("#update").click(function() {
+	counter ++
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
